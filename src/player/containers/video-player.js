89263ba3340b28dd470f0f45f67b2c3cbddb77e5
Juan Sifontez <juansfontz@gmail.com>
2017-12-30 04:51:03 -0400

    class 33 time elapsed added to video component

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -5,11 +5,14 @@ import Title from '../components/title'
 import PlayPause from '../components/play-pause'
 import Timer from '../components/timer'
 import Controls from '../components/video-player-controls.js'
+import formattedTime from '../components/utilities.js'
+
 
 class VideoPlayer extends Component {
   state = {
     pause: true,
     duration: 0,
+    currentTime: 0,
   }
   togglePlay = (event) => {
     this.setState({
@@ -26,8 +29,15 @@ class VideoPlayer extends Component {
   handleLoadedMetadata = event => {
     this.video = event.target;
     this.setState({
-      duration: this.video.duration
+      duration: formattedTime(this.video.duration)
     });
+  }
+
+  handleTimeUpdate = event => {
+    // console.log(this.video.currentTime)
+    this.setState({
+      currentTime: formattedTime(this.video.currentTime)
+    })
   }
 
   render() {
@@ -43,13 +53,15 @@ class VideoPlayer extends Component {
           />
           <Timer
             duration={this.state.duration}
+            currentTime={this.state.currentTime}
           /> 
         </Controls>
         <Video 
           autoplay={this.props.autoplay}
           pause={this.state.pause}
-          handleLoadedMetadata={this.handleLoadedMetadata}
           src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
+          handleLoadedMetadata={this.handleLoadedMetadata}
+          handleTimeUpdate={this.handleTimeUpdate}
         />
       </VideoPlayerLayout>
     )
