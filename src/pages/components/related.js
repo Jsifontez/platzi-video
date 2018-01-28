2b0ee504316c0906e9df845ec1bc92607db77952
Juan Sifontez <juansfontz@gmail.com>
2018-01-28 17:28:11 -0400

    fix: logo production

--- src/pages/components/related.js
+++ src/pages/components/related.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import logo from 'https://jsifontz.github.io/platzi-video/images/logo.png'
+import logo from '../../../images/logo.png'
 import './related.css'
 
 function Related(props) {
 	return(
 		<div className="Related">
-			<img src={logo} width={250} />
+			<img src='https://jsifontz.github.io/platzi-video/images/logo.png' width={250} />
 		</div>
 	)
 }
