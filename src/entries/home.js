05a737bf4c849a3051d3d30ad512e20045c4c49a
Juan Sifontez <juansfontz@gmail.com>
2018-04-19 10:59:04 -0400

    31 Middlewares

--- src/entries/home.js
+++ src/entries/home.js
@@ -1,11 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
 import Home from '../pages/containers/home'
-// import Categories from './src/playlist/components/categories'
-// import data from '../api.json'
-// import data from '../schemas/index.js'
 
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from '../reducers/index.js'
 
@@ -26,10 +23,38 @@ import { Map as map} from 'immutable'
 //   }
 // }
 
+// middlewares en redux
+
+  //ES5 way
+
+// function logger ( {getState, dispatch}) {
+//   // return (método para despachar el siguiente middleware) => {
+//   return (next) => {
+//     return (action) => {
+//       console.log('este es mi viejo estado', getState().toJS())
+//       console.log('vamos a enviar esta acción', action)
+//       const value = next(action)
+//       console.log('este es mi nuevo estado', getState().toJS())
+//       return value
+//     }
+//   }
+// }
+
+  //ES6 way
+
+const logger = ({getState, dispatch}) => next => action => {
+  console.log('estado anterior:', getState().toJS())
+  console.log('enviado acción', action)
+  const value = next(action)
+  console.log('nuevo estado:', getState().toJS())
+  return value
+}
+
 const store = createStore(
   reducer,
   map(),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  applyMiddleware(logger)
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
 // console.log(store.getState())
