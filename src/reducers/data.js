04fd63c44e741408e273911c12de46e6e8ba1dc2
Juan Sifontez <juansfontz@gmail.com>
2018-04-19 08:15:11 -0400

    30 Action Types

--- src/reducers/data.js
+++ src/reducers/data.js
@@ -1,6 +1,8 @@
 import schema from '../schemas/index.js'
 import { fromJS } from 'immutable'
 
+import { SEARCH_ENTITIES } from '../action-types/index'
+
 const initialState = fromJS({
   entities: schema.entities,
   categories: schema.result.categories,
@@ -9,7 +11,7 @@ const initialState = fromJS({
 
 function data(state = initialState, action) {
   switch (action.type) {
-    case 'SEARCH_ENTITIES': {
+    case SEARCH_ENTITIES: {
       // forma con immutable de consumir el search
 
       return state.set('search', action.payload.query)
